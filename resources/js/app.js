858510aaaa40b79e37f4f65e5f4b602af72bc4b4
javier ovalles <javier.ovalles.ing@gmail.com>
2023-08-16 18:35:31 -0400

    cambios antes de ejecutar migracion de usuario

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
+import '../css/app.scss';
+import Swal from 'sweetalert2';
+import jQuery from 'jquery';
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-dt/css/jquery.dataTables.css';
+
+window.Swal = Swal;
+window.$ = jQuery;
+window.DataTable = DataTable;
---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.scss','resources/js/app.js'])   
+    
+    
+   
+</head>
+<body>
+    <x-navegation/>
+    <main class="content">
+        {{$slot}}
+    </main>
+    <footer>
+        <script type="module">
+            let table = new DataTable('#table_id', {"pageLength":3,
+                lengthMenu:[
+                    [3,10,25,50],
+                    [3,10,25,50]
+                ],
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/1.13.1/i18n/es-ES.json"
+                }});
+        </script>
+
+    </footer>
+</body>
+</html>
